2eba2c72ab7004355709b48f6d39ee470121a181
zack <musyokaisaac98@gmail.com>
2022-11-02 23:20:38 +0300

    initialise setup tailwind header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+
+function Header() {
+  return (
+    <header className="">
+        <h1 className="text-2xl font-semibold"> 
+        This is the header
+        </h1>
+
+         </header>
+  )
+}
+
+export default Header
